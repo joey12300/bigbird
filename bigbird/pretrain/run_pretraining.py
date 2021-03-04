742488e1d2b5b9c6191a0f21c7d28c84dbf5e133
zhoushunjie <zhoushunjie@baidu.com>
2021-03-04 06:48:43 +0000

    update pretrain

--- bigbird/pretrain/run_pretraining.py
+++ bigbird/pretrain/run_pretraining.py
@@ -27,12 +27,16 @@ import numpy as np
 import tensorflow.compat.v2 as tf
 import tensorflow_datasets as tfds
 import tensorflow_text as tft
+from tensorflow.python.framework import ops
+from tensorflow.python.training.session_run_hook import SessionRunArgs
+
+import six
 
 import sentencepiece as spm
 
 
 FLAGS = flags.FLAGS
-
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.WARN)
 ## Required parameters
 
 flags.DEFINE_string(
@@ -122,6 +126,118 @@ flags.DEFINE_bool(
     "use_nsp", False,
     "Whether to use next sentence prediction loss.")
 
+def _as_graph_element(obj):
+  """Retrieves Graph element."""
+  graph = ops.get_default_graph()
+  if not isinstance(obj, six.string_types):
+    if not hasattr(obj, "graph") or obj.graph != graph:
+      raise ValueError("Passed %s should have graph attribute that is equal "
+                       "to current graph %s." % (obj, graph))
+    return obj
+  if ":" in obj:
+    element = graph.as_graph_element(obj)
+  else:
+    element = graph.as_graph_element(obj + ":0")
+    # Check that there is no :1 (e.g. it's single output).
+    try:
+      graph.as_graph_element(obj + ":1")
+    except (KeyError, ValueError):
+      pass
+    else:
+      raise ValueError("Name %s is ambiguous, "
+                       "as this `Operation` has multiple outputs "
+                       "(at least 2)." % obj)
+  return element
+
+class PrintLossHook(tf.estimator.SessionRunHook):
+  """Calculate and report global_step/sec and examples/sec during runtime."""
+  def __init__(self,
+               tensors,
+               every_n_iter=1,
+               every_n_secs=None,
+               at_end=False,
+               formatter=None):
+    """Initializes a `LoggingTensorHook`.
+    Args:
+      tensors: `dict` that maps string-valued tags to tensors/tensor names, or
+        `iterable` of tensors/tensor names.
+      every_n_iter: `int`, print the values of `tensors` once every N local
+        steps taken on the current worker.
+      every_n_secs: `int` or `float`, print the values of `tensors` once every N
+        seconds. Exactly one of `every_n_iter` and `every_n_secs` should be
+        provided.
+      at_end: `bool` specifying whether to print the values of `tensors` at the
+        end of the run.
+      formatter: function, takes dict of `tag`->`Tensor` and returns a string.
+        If `None` uses default printing all tensors.
+    Raises:
+      ValueError: if `every_n_iter` is non-positive.
+    """
+    only_log_at_end = (
+        at_end and (every_n_iter is None) and (every_n_secs is None))
+    if (not only_log_at_end and
+        (every_n_iter is None) == (every_n_secs is None)):
+      raise ValueError(
+          "either at_end and/or exactly one of every_n_iter and every_n_secs "
+          "must be provided.")
+    if every_n_iter is not None and every_n_iter <= 0:
+      raise ValueError("invalid every_n_iter=%s." % every_n_iter)
+    if not isinstance(tensors, dict):
+      self._tag_order = tensors
+      tensors = {item: item for item in tensors}
+    else:
+      self._tag_order = sorted(tensors.keys())
+    self._tensors = tensors
+    self._formatter = formatter
+    self._timer = (
+        tf.compat.v1.train.NeverTriggerTimer() if only_log_at_end else tf.compat.v1.train.SecondOrStepTimer(
+            every_secs=every_n_secs, every_steps=every_n_iter))
+    self._log_at_end = at_end
+    self._every_n_iter=every_n_iter
+
+  def begin(self):
+    self._timer.reset()
+    self._iter_count = 0
+    # Convert names to tensors if given
+    self._current_tensors = {
+        tag: _as_graph_element(tensor)
+        for (tag, tensor) in self._tensors.items()
+    }
+
+  def before_run(self, run_context):  # pylint: disable=unused-argument
+    self._should_trigger = self._timer.should_trigger_for_step(self._iter_count)
+    if self._should_trigger:
+      return SessionRunArgs(self._current_tensors)
+    else:
+      return None
+
+  def _log_tensors(self, tensor_values):
+    original = np.get_printoptions()
+    np.set_printoptions(suppress=True)
+    elapsed_secs, _ = self._timer.update_last_triggered_step(self._iter_count)
+    if self._formatter:
+      logging.info(self._formatter(tensor_values))
+    else:
+      stats = []
+      for tag in self._tag_order:
+        stats.append("%s = %s" % (tag, tensor_values[tag]))
+      if self._every_n_iter is not None and elapsed_secs is not None:
+        logging.warn("%s speed: (%.3f step/s)", ", ".join(stats), self._every_n_iter / elapsed_secs)
+      else:
+        logging.warn("%s", ", ".join(stats))
+    np.set_printoptions(**original)
+
+  def after_run(self, run_context, run_values):
+    _ = run_context
+    if self._should_trigger:
+      self._log_tensors(run_values.results)
+
+    self._iter_count += 1
+
+  def end(self, session):
+    if self._log_at_end:
+      values = session.run(self._current_tensors)
+      self._log_tensors(values)
 
 def input_fn_builder(data_dir, vocab_model_file, masked_lm_prob,
                      max_encoder_length, max_predictions_per_seq,
@@ -400,14 +516,15 @@ def model_fn_builder(bert_config):
 
       gradients = optimizer.compute_gradients(total_loss, tvars)
       train_op = optimizer.apply_gradients(gradients, global_step=global_step)
-
+      #logging_hook = tf.compat.v1.train.LoggingTensorHook({"loss" : total_loss}, every_n_iter=1)
+      logging_hook = PrintLossHook({"global_step":global_step, "loss" : total_loss})
       output_spec = tf.compat.v1.estimator.tpu.TPUEstimatorSpec(
           mode=mode,
           loss=total_loss,
           train_op=train_op,
+          training_hooks=[logging_hook],
           host_call=utils.add_scalars_to_summary(
               bert_config["output_dir"], {"learning_rate": learning_rate}))
-
     elif mode == tf.estimator.ModeKeys.EVAL:
 
       def metric_fn(masked_lm_loss_value, masked_lm_log_probs, masked_lm_ids,
@@ -491,7 +608,7 @@ class MaskedLMLayer(tf.compat.v1.layers.Layer):
 
   @property
   def trainable_weights(self):
-    self._trainable_weights = (self.extra_layer +
+    self._trainable_weights = (self.extra_layer.trainable_weights +
                                self.norm_layer.trainable_weights +
                                [self.output_bias])
     return self._trainable_weights
